d7392d2e6d69e4ce1eb8e9417920581736801dae
Noah <nallephu12@gmail.com>
2021-11-19 15:12:38 +0100

    init v2

--- testprogram1.s
+++ testprogram1.s
@@ -1,21 +1,14 @@
 .data
-<<<<<<< HEAD:testprogram.s
-message:    .asciz "9"
-isEqual:    .asciz "is Equal\n"
-=======
 message:    .asciz ""
->>>>>>> 21d0a4d610d31b60c7ae77b317a51c2c7f20da10:testprogram1.s
 
 .text
 .global main
 
 main:
-    movq $message, %rsi
-    cmpb $'9', (%rsi)
-    je print
-    ret
-
-print:
-    movq $isEqual, %rdi
+    movq $message, %rdi
+    movq $6, %rsi
+    movq stdin, %rdx
+    call fgets
+    movq $message, %rdi
     call puts
     ret
