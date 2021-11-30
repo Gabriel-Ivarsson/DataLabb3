527d0a188057cc5befae77e32d030b681734f0ed
Gabriel Ivarsson <gabriel@pop-os.localdomain>
2021-11-30 11:38:23 +0100

    putInt done in iterateDigit.s

--- iterateDigit.s
+++ iterateDigit.s
@@ -1,32 +1,53 @@
 .data
     buf:    .space 64
-    zero:   .asciz "Is zero\n"
-    one:    .asciz "is one\n"
+    numOfDigits:    .quad 0
     temp:   .space 64
+    num:    .quad 0
 
 .global main
 .text
 main:
     leaq buf, %r13
-    movq $101111, %r12
+    leaq temp, %r15
+    movq $10112, %rdi
+    movq %rdi, num
+    movq num, %r12
     call printEachDigit
-    movq $0, %rsi
+    movq $numOfDigits, %rsi
 
 printEachDigit:
     movq $0, %rdx
     movq %r12, %rax 
     movq $10, %r14
-    divq %r14 
+    divq %r14
     movq %rax, %r12 
     addq $'0', %rdx
     movq %rdx, (%r13)
-    incq %rsi 
+    movq %rdx, (%r15)
     incq %r13
+    incq %r15
+    addq $1, %rsi
+    movq %rsi, numOfDigits
     cmpq $0, %rax 
-    je end
+    je transfer2Buf1
     jmp printEachDigit
+transfer2Buf1:
+    movq $'\0', (%r13)
+    movq $'\0', (%r15)
+    leaq buf, %r13
+    movq $7, %rsi
+    decq %r15
+    jmp transfer2Buf2
+transfer2Buf2:
+    mov (%r15), %edx
+    mov %edx, (%r13)
+    incq %r13
+    decq %r15
+    subq $1, %rsi
+    cmpq $0, %rsi
+    jg transfer2Buf2
+    jmp end
 end:
-    movb $0, (%r13)
     movq $buf, %rdi
     call puts
     ret
