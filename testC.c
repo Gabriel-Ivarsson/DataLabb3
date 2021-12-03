8c280184ddd0fd761dcbae9e34aa5f059fd3b31a
Noah <nallephu12@gmail.com>
2021-12-03 20:26:09 +0100

    added tests combining getText and getInt

--- testC.c
+++ testC.c
@@ -5,7 +5,6 @@
 int main()
 {
     // getInt
-    
     int pleaseWork = getInt();
     printf("Message 1 is %d\n", pleaseWork);
     pleaseWork = getInt();
@@ -14,22 +13,25 @@ int main()
     printf("inpos: %d\n", bajs);
 
     // getText
+    printf("setting inpos to 0\n");
+    setInPos(0);
 
-    // char word1[SIZE+2] = "";
-    // printf("Enter first word(max %d long):\n",SIZE);
-    // int number1 = getText(word1, SIZE);
+    char word1[SIZE+2] = "";
+    printf("Enter first word(max %d long):\n",SIZE);
+    int number1 = getText(word1, SIZE);
 
-    // printf("Compared number1 was: %d\n", number1);
-    // printf("Word1: %s\n", word1);
+    printf("Compared number1 was: %d\n", number1);
+    printf("Word1: %s\n", word1);
 
-    // setInPos(3);
+    printf("setting inpos to 2\n");
+    setInPos(2);
 
-    // printf("Enter second word(max %d long):\n",SIZE-2);
-    // char word2[SIZE] = "";
-    // int number2 = getText(word2, SIZE-2);
+    printf("Enter second word(max %d long):\n",SIZE-2);
+    char word2[SIZE] = "";
+    int number2 = getText(word2, SIZE-2);
 
-    // printf("Compared number2 was: %d\n", number2);
-    // printf("Word2: %s\n", word2);
+    printf("Compared number2 was: %d\n", number2);
+    printf("Word2: %s\n", word2);
 
-    // return 0;
+    return 0;
 }
